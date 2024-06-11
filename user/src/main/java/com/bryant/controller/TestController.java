8d9dd63264d24527d9ed355c63063c5f46eb1391
bryantmo <bryantmo@tencent.com>
2024-06-11 15:53:41 +0800

    test

---
+++ user/src/main/java/com/bryant/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.bryant.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    /**
+     * 除了通过eValue注解绑定注入之外，也可以通过Environment 对象来获取配置 属性，比如:注入Environment
+     */
+//    @Value("${from}")
+//    private String from;
+
+    @Autowired
+    private Environment environment;
+
+//    @GetMapping("/from_test")
+//    public String getFrom() {
+//        return "Hello from " + from;
+//    }
+
+    @GetMapping("/from_test2")
+    public String getFrom2() {
+        return "Hello from " + environment.getProperty("from", "undefined");
+    }
+
+}
