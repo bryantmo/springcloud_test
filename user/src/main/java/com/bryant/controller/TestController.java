6961cf4f7b204e3c6506e8d71c4c8289f2b8825b
mmbscau <1443660673@qq.com>
2024-06-26 10:06:23 +0800

    1、config客户端 开放config客户端的依赖和端点自动刷新接口 @RefreshScope注解到接口上

--- user/src/main/java/com/bryant/controller/TestController.java
+++ user/src/main/java/com/bryant/controller/TestController.java
@@ -2,10 +2,12 @@ package com.bryant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class TestController {
 
