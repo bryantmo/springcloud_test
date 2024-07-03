097a05154b926e996fe84ee0e201dff918fb1658
mmbscau <1443660673@qq.com>
2024-07-03 11:39:40 +0800

    1、springCloudEureka 服务端-eureka.instance相关配置项补充 2、springCloudEureka-client 客户端-开启健康检查、暴露端点health

--- user/src/main/java/com/bryant/UserServer.java
+++ user/src/main/java/com/bryant/UserServer.java
@@ -3,9 +3,9 @@ package com.bryant;
 import com.bryant.import_package.EnableSpringStudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 // 测试自定义启动类加载
 @EnableSpringStudy
