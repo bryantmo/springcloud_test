f5e0fbb29ac0664c06bba71b80ac5340adac5721
mmbscau <1443660673@qq.com>
2024-08-14 20:25:13 +0800

    1、客户端限流测试Guava

--- webdoor/src/main/java/com/bryant/config/constraint/WebRequestMappingRegistrationsConfig.java
+++ webdoor/src/main/java/com/bryant/config/constraint/WebRequestMappingRegistrationsConfig.java
@@ -1,35 +1,35 @@
-package com.bryant.config.constraint;
-
-import com.bryant.config.constraint.request_mapping.WebRequestMappingHandlerMapping;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-
-/**
- * 重写 spring mvc 中 {@link RequestMappingHandlerMapping}
- * @see RequestMappingHandlerMapping
- * @see com.bryant.config.constraint.request_mapping.AbstractRequestMappingHandlerMapping
- */
-@Configuration
-public class WebRequestMappingRegistrationsConfig implements WebMvcRegistrations {
-
-    @Override
-    public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
-        WebRequestMappingHandlerMapping requestMappingHandlerMapping = new WebRequestMappingHandlerMapping();
-        requestMappingHandlerMapping.setPathMatcher(routerPathConstraintMatcher());
-        return requestMappingHandlerMapping;
-    }
-
-    @Override
-    public RequestMappingHandlerAdapter getRequestMappingHandlerAdapter() {
-        return new ExtendedRequestMappingHandlerAdapter();
-    }
-
-    @Bean
-    public WebRouterPathConstraintMatcher routerPathConstraintMatcher() {
-        return new WebRouterPathConstraintMatcher();
-    }
-
-}
+//package com.bryant.config.constraint;
+//
+//import com.bryant.config.constraint.request_mapping.WebRequestMappingHandlerMapping;
+//import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+//import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+//
+///**
+// * 重写 spring mvc 中 {@link RequestMappingHandlerMapping}
+// * @see RequestMappingHandlerMapping
+// * @see com.bryant.config.constraint.request_mapping.AbstractRequestMappingHandlerMapping
+// */
+//@Configuration
+//public class WebRequestMappingRegistrationsConfig implements WebMvcRegistrations {
+//
+//    @Override
+//    public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+//        WebRequestMappingHandlerMapping requestMappingHandlerMapping = new WebRequestMappingHandlerMapping();
+//        requestMappingHandlerMapping.setPathMatcher(routerPathConstraintMatcher());
+//        return requestMappingHandlerMapping;
+//    }
+//
+//    @Override
+//    public RequestMappingHandlerAdapter getRequestMappingHandlerAdapter() {
+//        return new ExtendedRequestMappingHandlerAdapter();
+//    }
+//
+//    @Bean
+//    public WebRouterPathConstraintMatcher routerPathConstraintMatcher() {
+//        return new WebRouterPathConstraintMatcher();
+//    }
+//
+//}
---
+++ webdoor/src/main/java/com/bryant/controller/ratelimit/GuavaLimitController.java
@@ -0,0 +1,39 @@
+package com.bryant.controller.ratelimit;
+
+import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/guava")
+@Slf4j
+public class GuavaLimitController {
+
+    private static final Integer RATE_LIMIT = 10;
+    RateLimiter rateLimiter = RateLimiter.create(RATE_LIMIT);
+
+    @GetMapping("/try_require_rate_limit")
+    public String hello() {
+        boolean limit = false;
+        try {
+            limit = rateLimiter.tryAcquire();
+        } catch (Exception e) {
+            log.error("limit error", e);
+        }
+
+        if (limit) {
+            return "hello guava rate limit success...";
+        }
+        throw new RuntimeException("hello guava rate limit fall...");
+    }
+
+    @GetMapping("/acquire_rate_limit")
+    public String hello2() {
+        double acquireTimeGap = rateLimiter.acquire();
+        return "hello guava rate limit success, acquireTimeGap: " + acquireTimeGap;
+    }
+
+
+}
