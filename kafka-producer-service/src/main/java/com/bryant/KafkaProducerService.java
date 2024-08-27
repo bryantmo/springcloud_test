fc54744e590dda837891930119146907b30df3d6
bryantmo <bryantmo@tencent.com>
2024-08-27 08:58:06 +0800

    添加kafka的生产者和消费者服务

--- kafka-producer-service/src/main/java/com/bryant/KafkaProducerService.java
+++ kafka-producer-service/src/main/java/com/bryant/KafkaProducerService.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaConsumerService {
+public class KafkaProducerService {
     public static void main(String[] args) {
-        SpringApplication.run(KafkaConsumerService.class, args);
+        SpringApplication.run(KafkaProducerService.class, args);
     }
 }
