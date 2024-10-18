1e713ca2ac5901fb5abf283540ff43c00d4fd260
bryantmo <1443660673@qq.com>
2024-10-18 08:41:21 +0800

    1、volatile验证

---
+++ webdoor/src/test/java/com/bryant/multic/TestVolatile.java
@@ -0,0 +1,12 @@
+package com.bryant.multic;
+
+public class TestVolatile {
+
+    private static Integer num = 110;
+
+    public static void main(String[] args) {
+        System.out.println(num);
+        num = 1111;
+
+    }
+}
